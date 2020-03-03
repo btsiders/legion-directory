6a470c492ddf0b76234ec00e5351be47272ce8ec
Bryan Siders <bts@sypher.com>
2020-03-03 00:18:28 -0500

    style

--- src/app/pages/garrisons/garrisons.page.ts
+++ src/app/pages/garrisons/garrisons.page.ts
@@ -1,29 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { MemberApiService } from 'src/app/services/member-api.service';
+import { MemberApiService } from 'src/app/shared/member-api.service';
 import { Unit } from 'src/app/shared/unit';
 import { BehaviorSubject } from 'rxjs';
 
 @Component({
-  selector: 'app-garrisons',
-  templateUrl: './garrisons.page.html',
-  styleUrls: ['./garrisons.page.scss'],
+    selector: 'app-garrisons',
+    templateUrl: './garrisons.page.html',
+    styleUrls: ['./garrisons.page.scss'],
 })
 export class GarrisonsPage implements OnInit {
 
-  units$ = new BehaviorSubject<Unit[]>(null);
+    units$ = new BehaviorSubject<Unit[]>(null);
 
-  constructor(
-    private memberApiService: MemberApiService
-  ) { }
+    constructor(
+        private memberApiService: MemberApiService
+    ) { }
 
-  ngOnInit() {
-    this.getUnits();
-  }
+    ngOnInit() {
+        this.getUnits();
+    }
 
-  private async getUnits() {
-    const units = await this.memberApiService.getUnits().toPromise();
-    this.units$.next(units);
-    console.log(units);
-  }
+    private async getUnits() {
+        const units = await this.memberApiService.getUnits().toPromise();
+        this.units$.next(units);
+    }
 
 }
