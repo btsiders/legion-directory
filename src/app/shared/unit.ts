af7464d5f5dd3a477ca5f6a427ab549363a3a8f7
Bryan Siders <bts@sypher.com>
2020-03-05 01:22:21 -0500

    added unitSize

--- src/app/shared/unit.ts
+++ src/app/shared/unit.ts
@@ -1,16 +1,18 @@
-import { Officer, Member } from './member-api.service';
+import { Officer } from './officer';
+import { Member } from './member';
 
 /**
  * Unit (Garrison/Outpost) Interface
- * 
+ *
  * Returns as array of 'garrisons' from garrisons call.
  * Returns as 'unit' from garrisons/# call.
  * Includes members list in garrisons/#/members call.
  */
 export interface Unit {
-    id: number;
+    id?: number;
     name: string;
     unitType: 'Garrison' | 'Outpost';
+    unitSize?: number;
     logo: string;
     url: string;
     officers: Officer[];
